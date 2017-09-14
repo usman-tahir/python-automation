78c6f1b9e26914c1258a7cbe4df0a0de189d3dc4
usman-tahir <tahir.usman.ali94@outlook.com>
2017-09-14 13:25:14 -0400

    feat: added the hello script from chapter 1

---
+++ chapter-1/hello/hello.py
@@ -0,0 +1,13 @@
+
+print('Hello world!')
+print('What is your name?')
+my_name = raw_input()
+
+print('Nice to meet you, %s' % my_name)
+print('The length of your name is:')
+print(len(my_name))
+
+print('What is your age?')
+my_age = int(raw_input())
+
+print('You will be %s years old, in one year.' % str(my_age + 1))
